3e9f7be8340be3cc05bc6d63f12efb6b88e8a6d2
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-01-10 19:45:04 +0530

    CORS Error Fixed

--- src/main/java/dev/moditha/DemoApplication.java
+++ src/main/java/dev/moditha/DemoApplication.java
@@ -2,7 +2,10 @@ package dev.moditha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {"dev.moditha.movies","dev.moditha.reviews"})
@@ -12,4 +15,17 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
